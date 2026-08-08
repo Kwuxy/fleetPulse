5d2476271809733636ce932d97bddc7108a9b060
Kwuxy <guenin.vincent4@gmail.com>
2026-08-08 17:38:38 +0200

    [Add] Create & list trucks

---
+++ services/fleet_service/app/services/truck_service.py
@@ -0,0 +1,38 @@
+import uuid
+
+from services.fleet_service.app.exceptions import InvalidTruckCapacity, InvalidPlateNumber
+from services.fleet_service.app.models.truck import CreateTruckRequest, Truck, TruckStatus
+
+trucks: dict[str, Truck] = {}
+
+
+def create_truck(create_truck_request: CreateTruckRequest):
+    if not is_valid_capacity(create_truck_request.capacity_kg):
+        raise InvalidTruckCapacity(create_truck_request.capacity_kg)
+
+    if not is_valid_plate_number(create_truck_request.plate_number):
+        raise InvalidPlateNumber(create_truck_request.plate_number)
+
+    # Should validate plate number uniqueness here
+
+    truck = Truck(
+        id=generate_truck_id(),
+        **create_truck_request.model_dump(),
+        status=TruckStatus.AVAILABLE
+    )
+
+    trucks[truck.id] = truck
+
+    return truck
+
+def generate_truck_id():
+    return f"truck-{uuid.uuid4().hex[:8]}"
+
+def is_valid_plate_number(plate_number: str):
+    return True  # TODO : Implement plate number validation
+
+def is_valid_capacity(capacity_kg: int):
+    return capacity_kg > 0
+
+def get_trucks() -> list[Truck]:
+    return list(trucks.values())
