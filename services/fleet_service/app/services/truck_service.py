c6b9cce5e66daf2254372724d3603da56c4b9ca0
Kwuxy <guenin.vincent4@gmail.com>
2026-08-08 17:52:32 +0200

    [Upd] Those functions should be private

--- services/fleet_service/app/services/truck_service.py
+++ services/fleet_service/app/services/truck_service.py
@@ -7,16 +7,16 @@ trucks: dict[str, Truck] = {}
 
 
 def create_truck(create_truck_request: CreateTruckRequest):
-    if not is_valid_capacity(create_truck_request.capacity_kg):
+    if not _is_valid_capacity(create_truck_request.capacity_kg):
         raise InvalidTruckCapacity(create_truck_request.capacity_kg)
 
-    if not is_valid_plate_number(create_truck_request.plate_number):
+    if not _is_valid_plate_number(create_truck_request.plate_number):
         raise InvalidPlateNumber(create_truck_request.plate_number)
 
     # Should validate plate number uniqueness here
 
     truck = Truck(
-        id=generate_truck_id(),
+        id=_generate_truck_id(),
         **create_truck_request.model_dump(),
         status=TruckStatus.AVAILABLE
     )
@@ -25,13 +25,13 @@ def create_truck(create_truck_request: CreateTruckRequest):
 
     return truck
 
-def generate_truck_id():
+def _generate_truck_id():
     return f"truck-{uuid.uuid4().hex[:8]}"
 
-def is_valid_plate_number(plate_number: str):
+def _is_valid_plate_number(plate_number: str):
     return True  # TODO : Implement plate number validation
 
-def is_valid_capacity(capacity_kg: int):
+def _is_valid_capacity(capacity_kg: int):
     return capacity_kg > 0
 
 def get_trucks() -> list[Truck]:
