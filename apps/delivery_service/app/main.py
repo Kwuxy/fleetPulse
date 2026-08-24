2a269e2e55975d27eba6a0f06e5af93026560c65
Kwuxy <guenin.vincent4@gmail.com>
2026-08-24 20:24:32 +0200

    [Add] Logger

--- apps/delivery_service/app/main.py
+++ apps/delivery_service/app/main.py
@@ -1,9 +1,17 @@
+import logging
+import os
 from contextlib import asynccontextmanager
 from typing import AsyncIterator
 
 from fastapi import FastAPI
 from app.routes import delivery_routes
 from app.clients import kafka_client
+
+logging.basicConfig(
+    level=os.environ.get("LOG_LEVEL", "INFO"),
+    # format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    format=f"{'%(levelname)s:': <18} %(message)s",
+)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
--- apps/fleet_service/app/main.py
+++ apps/fleet_service/app/main.py
@@ -1,6 +1,14 @@
+import logging
+import os
 from fastapi import FastAPI
 
 from app.routes import truck_routes, assignment_routes
+
+logging.basicConfig(
+    level=os.environ.get("LOG_LEVEL", "INFO"),
+    # format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    format=f"{'%(levelname)s:': <18} %(message)s",
+)
 
 app = FastAPI(title='Fleet Service')
 
