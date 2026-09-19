73d7a0e31dec581c113d032ba08cf89b49992b3c
Kwuxy <guenin.vincent4@gmail.com>
2026-09-19 21:42:11 +0200

    [Add] Implement departure producer

--- apps/delivery_service/app/producers/departure_producer.py
+++ apps/delivery_service/app/producers/departure_producer.py
@@ -1,9 +1,17 @@
+import functools
 import logging
 
 from app.models.truck_departure import TruckDepartureScheduled
+from app.clients import kafka_client
 
 logger = logging.getLogger(__name__)
 
 
 async def produce_truck_departure_scheduled(request: TruckDepartureScheduled) -> None:
-    pass
+    topic = 'truck-departure-scheduled'
+    future = await kafka_client.get_producer().send(
+        topic,
+        key=request.delivery_id,
+        value=request.model_dump(mode="json"),
+    )
+    future.add_done_callback(functools.partial(kafka_client.log_send_failure, topic, request.delivery_id))
